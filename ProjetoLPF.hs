970628564ce59d167852266f2040400e5bcdf24b
Estyvison2 <39842092+Estyvison2@users.noreply.github.com>
2018-06-01 18:33:31 -0300

    Projeto
    Começando

---
+++ ProjetoLPF.hs
@@ -0,0 +1,34 @@
+{-
+    Projeto LPF - Ifood
+    Alunos: Daniel Almeida
+            Estyvison Linhares
+            Kelly Bezerra
+            Paulo Guilherme
+-}
+
+--Módulos:
+import Control.Exception
+import System.IO
+import System.IO.Error
+import Control.Monad
+
+
+--definição de tipos de dados
+type Clientes = [Cliente]
+type Nome = String
+type Cpf = String
+type Endereco = String
+type Fone = String
+type Preco = Float
+type Bebidas = [(String, Preco)]
+type Sobremesas = [(String, Preco)]
+type Petiscos = [(String, Preco)]
+type Principal = [(String, Preco)]
+type Pedidos = [(String, Preco)]
+
+data Cliente = Cliente Nome Cpf Endereco Fone
+               deriving(Show, Read, Eq)
+data Restaurante = Restaurante Nome Endereco Cardapio Clientes Pedidos
+                   deriving(Show, Read, Eq)
+data Cardapio = Cardapio Bebidas Sobremesas Petiscos Principal
+                deriving(Show, Read, Eq)
