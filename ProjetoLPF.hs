0d2a1546e179881d358ea623a9fc84727a483e0a
Estyvison2 <39842092+Estyvison2@users.noreply.github.com>
2018-06-02 19:55:03 -0300

    Tito
    Inicio da main

--- ProjetoLPF.hs
+++ ProjetoLPF.hs
@@ -11,24 +11,57 @@ import Control.Exception
 import System.IO
 import System.IO.Error
 import Control.Monad
+import System.Process
 
-
+{--
 --definição de tipos de dados
 type Clientes = [Cliente]
 type Nome = String
 type Cpf = String
 type Endereco = String
 type Fone = String
+type Senha = String
 type Preco = Float
 type Bebidas = [(String, Preco)]
 type Sobremesas = [(String, Preco)]
 type Petiscos = [(String, Preco)]
 type Principal = [(String, Preco)]
-type Pedido = [(String, Preco)]
+type Pedidos = [(String, Preco)]
 
-data Cliente = Cliente Nome Cpf Endereco Fone
+data Cliente = Cliente Nome Cpf Endereco Fone Senha
                deriving(Show, Read, Eq)
 data Restaurante = Restaurante Nome Endereco Cardapio Clientes Pedidos
                    deriving(Show, Read, Eq)
 data Cardapio = Cardapio Bebidas Sobremesas Petiscos Principal
                 deriving(Show, Read, Eq)
+--}
+
+
+--função menu
+main = do
+       putStrLn "Bem-vindo ao aplicativo FoodFast! Realize a operação desejada: "
+       putStrLn "1-Cadastro"
+       putStrLn "2-Login"
+       putStrLn "3-Fechar aplicativo"
+       opcao <- getChar
+       if opcao == ' '
+         then return ()
+         else do
+           if opcao == '1'
+             then do
+               cadastro
+               main
+             else do
+               if opcao == '2'
+                 then do
+                   login
+                   main
+                 else return ()
+
+--função Cadastro
+cadastro = do
+        putStrLn "Castro feito com sucesso"
+
+ --função Login
+login = do
+        putStrLn "Login feito com sucesso"
